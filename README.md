68d04fd047bc6a7979f1f380bf3f4ecb8763751a
gustavocassa <gustavocassanello2@gmail.com>
2024-01-18 00:16:36 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# projectoAUF2ba
---
+++ js/scripts.js
@@ -0,0 +1,50 @@
+$(document).ready(function() {
+    $('.contenedorLlamados1').click(function() {
+        // Encuentra todos los enlaces dentro del div
+        var enlaces = $(this).find('a');
+
+        // Abre una nueva pestaña en el navegador para cada enlace
+        for (var i = 0; i < enlaces.length; i++) {
+            window.open(enlaces[i].href, '_blank');
+        }
+    });
+});
+
+$(document).ready(function() {
+    $('.contenedorLlamados2').click(function() {
+        // Encuentra todos los enlaces dentro del div
+        var enlaces = $(this).find('a');
+
+        // Abre una nueva pestaña en el navegador para cada enlace
+        for (var i = 0; i < enlaces.length; i++) {
+            window.open(enlaces[i].href, '_blank');
+        }
+    });
+});
+
+$(document).ready(function() {
+    $('.contenedorLlamados3').click(function() {
+        // Encuentra todos los enlaces dentro del div
+        var enlaces = $(this).find('a');
+
+        // Abre una nueva pestaña en el navegador para cada enlace
+        for (var i = 0; i < enlaces.length; i++) {
+            window.open(enlaces[i].href, '_blank');
+        }
+    });
+});
+
+$(document).ready(function() {
+    $('.contenedorLlamados4').click(function() {
+        // Encuentra todos los enlaces dentro del div
+        var enlaces = $(this).find('a');
+
+        // Abre una nueva pestaña en el navegador para cada enlace
+        for (var i = 0; i < enlaces.length; i++) {
+            window.open(enlaces[i].href, '_blank');
+        }
+    });
+});
+
+
+//<li class="barra-menu"><a href="#barras"class="menu1">☰</a></li>
